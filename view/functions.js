d1daa3912f15b72c0e68493807156df27a741bc9
Yusra Salam <yusra.salam126@gmail.com>
2022-02-06 04:28:35 -0500

    edit

--- view/functions.js
+++ view/functions.js
@@ -1,4 +1,4 @@
 function getCategories(){
-    document.getElementById("category").innerHTML = "<button>SNRIs</button><button>SSRIs</button><button>DRIs</button><button>Stimulants</button><button>Non-Stimulants</button><button>Mood-Stabilizers</button> ";
+    document.getElementById("category").innerHTML = `<button id="snris">SNRIs</button><button id="ssris">SSRIs</button><button id="dris">DRIs</button><button id="stimulants">Stimulants</button><button id="nonStimulants">Non-Stimulants</button><button id="moodstabilizers">Mood-Stabilizers</button> `;
 }
 
